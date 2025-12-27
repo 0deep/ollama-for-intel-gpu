5a8ef2547e760614f0c0b11e9f0c939389a4b2c1
0deep <kimexpon@gmail.com>
2025-12-27 15:54:46 +0900

    feat: Add CI/CD workflows

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,9 +1,3 @@
-#!powershell
-#
-# powershell -ExecutionPolicy Bypass -File .\scripts\build_windows.ps1
-#
-# gcloud auth application-default login
-
 $ErrorActionPreference = "Stop"
 
 mkdir -Force -path .\dist | Out-Null
@@ -25,35 +19,6 @@ function checkEnv {
     write-host "Locating required tools and paths"
     $script:SRC_DIR=$PWD
 
-    # Locate CUDA versions
-    $cudaList=(get-item "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v*\bin\" -ea 'silentlycontinue')
-    if ($cudaList.length -eq 0) {
-        $d=(get-command -ea 'silentlycontinue' nvcc).path
-        if ($null -ne $d) {
-            $script:CUDA_DIRS=@($d| split-path -parent)
-        }
-    } else {
-        # Favor newer patch versions if available
-        $script:CUDA_DIRS=($cudaList | sort-object -Descending)
-    }
-    if ($script:CUDA_DIRS.length -gt 0) {
-        write-host "Available CUDA Versions: $script:CUDA_DIRS"
-    } else {
-        write-host "No CUDA versions detected"
-    }
-
-    # Locate ROCm version
-    if ($null -ne $env:HIP_PATH) {
-        $script:HIP_PATH=$env:HIP_PATH
-    } else {
-        $script:HIP_PATH=(get-item "C:\Program Files\AMD\ROCm\*\bin\" -ea 'silentlycontinue' | sort-object -Descending)
-    }
-    
-    $inoSetup=(get-item "C:\Program Files*\Inno Setup*\")
-    if ($inoSetup.length -gt 0) {
-        $script:INNO_SETUP_DIR=$inoSetup[0]
-    }
-
     $script:DIST_DIR="${script:SRC_DIR}\dist\windows-${script:TARGET_ARCH}"
     $env:CGO_ENABLED="1"
     Write-Output "Checking version"
@@ -73,19 +38,6 @@ function checkEnv {
         $script:PKG_VERSION="0.0.0"
     }
     write-host "Building Ollama $script:VERSION with package version $script:PKG_VERSION"
-
-    # Note: Windows Kits 10 signtool crashes with GCP's plugin
-    if ($null -eq $env:SIGN_TOOL) {
-        ${script:SignTool}="C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"
-    } else {
-        ${script:SignTool}=${env:SIGN_TOOL}
-    }
-    if ("${env:KEY_CONTAINER}") {
-        ${script:OLLAMA_CERT}=$(resolve-path "${script:SRC_DIR}\ollama_inc.crt")
-        Write-host "Code signing enabled"
-    } else {
-        write-host "Code signing disabled - please set KEY_CONTAINERS to sign and copy ollama_inc.crt to the top of the source tree"
-    }
     $script:JOBS=([Environment]::ProcessorCount)
 }
 
@@ -99,7 +51,6 @@ function sycl {
     & cmake --install build\sycl --component SYCL --strip
     if ($LASTEXITCODE -ne 0) { exit($LASTEXITCODE)}
 }
-
 
 
 function ollama {
@@ -175,33 +126,6 @@ function deps {
     write-host "Done."
 }
 
-function sign {
-    if ("${env:KEY_CONTAINER}") {
-        write-host "Signing Ollama executables, scripts and libraries"
-        & "${script:SignTool}" sign /v /fd sha256 /t http://timestamp.digicert.com /f "${script:OLLAMA_CERT}" `
-            /csp "Google Cloud KMS Provider" /kc ${env:KEY_CONTAINER} `
-            $(get-childitem -path "${script:SRC_DIR}\dist\windows-*" -r -include @('*.exe', '*.dll'))
-        if ($LASTEXITCODE -ne 0) { exit($LASTEXITCODE)}
-    } else {
-        write-host "Signing not enabled"
-    }
-}
-
-function installer {
-    if ($null -eq ${script:INNO_SETUP_DIR}) {
-        write-host "ERROR: missing Inno Setup installation directory - install from https://jrsoftware.org/isdl.php"
-        exit 1
-    }
-    write-host "Building Ollama Installer"
-    cd "${script:SRC_DIR}\app"
-    $env:PKG_VERSION=$script:PKG_VERSION
-    if ("${env:KEY_CONTAINER}") {
-        & "${script:INNO_SETUP_DIR}\ISCC.exe" /DARCH=$script:TARGET_ARCH /SMySignTool="${script:SignTool} sign /fd sha256 /t http://timestamp.digicert.com /f ${script:OLLAMA_CERT} /csp `$qGoogle Cloud KMS Provider`$q /kc ${env:KEY_CONTAINER} `$f" .\ollama.iss
-    } else {
-        & "${script:INNO_SETUP_DIR}\ISCC.exe" /DARCH=$script:TARGET_ARCH .\ollama.iss
-    }
-    if ($LASTEXITCODE -ne 0) { exit($LASTEXITCODE)}
-}
 
 function zip {
     if (Test-Path -Path "${script:SRC_DIR}\dist\windows-amd64") {
@@ -227,8 +151,6 @@ try {
         ollama
         app
         deps
-        sign
-        installer
         zip
     } else {
         for ( $i = 0; $i -lt $args.count; $i++ ) {
